992b0ffd9f01ba6201b4a50ef29bc78d035c2ef4
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-03-29 00:09:07 +0100

    Implement base wrappers

--- pykuda2/base.py
+++ pykuda2/base.py
@@ -1,6 +1,41 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from enum import Enum
-from typing import Optional
+from json import JSONDecodeError
+from typing import Optional, Union
+from httpx import codes as HTTP_STATUS_CODE
+
+__version__ = "0.1.0"
+
+from uuid import uuid4
+
+import httpx
+
+from pykuda2.exceptions import (
+    UnsupportedHTTPMethodException,
+    ConnectionException,
+    InvalidResponseException,
+    TokenException,
+)
+
+
+@dataclass
+class APIResponse:
+    status_code: int
+    status: Optional[str]
+    message: Optional[str]
+    data: Optional[dict]
+    raw: Union[list, dict]
+
+
+class HTTPMethod(str, Enum):
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    PATCH = "PATCH"
+    DELETE = "DELETE"
+    OPTIONS = "OPTIONS"
+    HEAD = "HEAD"
 
 
 class Mode(str, Enum):
@@ -56,6 +91,14 @@ class AbstractAPIWrapper(ABC):
         ...
 
     @property
+    def base_headers(self) -> dict:
+        return {
+            "accept": "application/json; charset=utf-8",
+            "content-type": "application/json",
+            "user-agent": f"PyKuda {__version__}",
+        }
+
+    @property
     def base_url(self) -> str:
         return {
             Mode.DEVELOPMENT: "http://kuda-openapi-uat.kudabank.com/v2.1",
@@ -63,8 +106,35 @@ class AbstractAPIWrapper(ABC):
         }[self.mode]
 
     @abstractmethod
-    def api_call(self):
+    def api_call(self, service_type: ServiceType, data: dict, method=HTTPMethod.POST):
         ...
+
+    def _parse_call_kwargs(self, service_type: ServiceType, data: dict) -> dict:
+        payload = {
+            "ServiceType": service_type,
+            "RequestRef": str(uuid4()),
+            "Data": data,
+        }
+        return {
+            "url": self.base_url,
+            "json": payload,
+            "headers": self.headers,
+        }
+
+    def _parse_response(self, response: httpx.Response) -> APIResponse:
+        try:
+            response_body = response.json()
+            return APIResponse(
+                status_code=response.status_code,
+                status=response_body.get("Status") or response_body.get("status"),
+                message=response_body.get("Message") or response_body.get("message"),
+                data=response_body.get("Data") or response_body.get("data"),
+                raw=response_body,
+            )
+        except JSONDecodeError:
+            raise InvalidResponseException(
+                f"Unable to decode response. STATUS_CODE: {response.status_code}"
+            )
 
 
 class APIWrapper(AbstractAPIWrapper):
@@ -76,16 +146,124 @@ class APIWrapper(AbstractAPIWrapper):
         if self._token:
             return self._token
         else:
-            ...
+            token_url = f"{self.base_url}/Account/GetToken"
+            auth_data = {"email": self.email, "apiKey": self.api_key}
+            try:
+                response = httpx.post(
+                    url=token_url, json=auth_data, headers=self.base_headers
+                )
+            except httpx.ConnectError:
+                raise ConnectionException(
+                    "Unable to connect to server. Please ensure you have an internet connection"
+                )
+            except httpx.ConnectTimeout:
+                raise ConnectionException("Server refused to respond")
+            if response.status_code == HTTP_STATUS_CODE.OK:
+                return response.text
+            raise TokenException(
+                "Unable to get access token, It's likely that you provided an invalid credential "
+                "or your apiKey has expired. You can always generate a new apiKey "
+                "from your developer account"
+            )
 
     @property
     def headers(self) -> dict:
-        return {}
+        return {**self.base_headers, "authorization": f"Bearer {self.token}"}
 
-    def api_call(self):
-        ...
+    def api_call(self, service_type: ServiceType, data: dict, method=HTTPMethod.POST):
+
+        http_method_call_kwargs = self._parse_call_kwargs(
+            service_type=service_type, data=data
+        )
+        http_methods_mapping = {
+            HTTPMethod.GET: httpx.get,
+            HTTPMethod.POST: httpx.post,
+            HTTPMethod.PUT: httpx.put,
+            HTTPMethod.PATCH: httpx.patch,
+            HTTPMethod.DELETE: httpx.delete,
+            HTTPMethod.OPTIONS: httpx.options,
+            HTTPMethod.HEAD: httpx.head,
+        }
+        http_method_callable = http_methods_mapping.get(method)
+        if not http_method_callable:
+            raise UnsupportedHTTPMethodException(
+                f"{method} is not a supported HTTP method"
+            )
+        try:
+            response = http_method_callable(**http_method_call_kwargs)
+            return self._parse_response(response)
+        except httpx.ConnectError:
+            raise ConnectionException(
+                "Unable to connect to server. Please ensure you have an internet connection"
+            )
+        except httpx.ConnectTimeout:
+            raise ConnectionException("Server refused to respond")
 
 
 class AsyncAPIWrapper(AbstractAPIWrapper):
     def __init__(self, email: str, api_key: str, mode=Mode.DEVELOPMENT):
         super().__init__(email=email, api_key=api_key, mode=mode)
+
+    @property
+    async def token(self) -> str:
+        if self._token:
+            return self._token
+        else:
+            token_url = f"{self.base_url}/Account/GetToken"
+            auth_data = {"email": self.email, "apiKey": self.api_key}
+            try:
+                async with httpx.AsyncClient() as client:
+                    response = await client.post(
+                        url=token_url, json=auth_data, headers=self.base_headers
+                    )
+            except httpx.ConnectError:
+                raise ConnectionException(
+                    "Unable to connect to server. Please ensure you have an internet connection"
+                )
+            except httpx.ConnectTimeout:
+                raise ConnectionException("Server refused to respond")
+            if response.status_code == HTTP_STATUS_CODE.OK:
+                return response.text
+            raise TokenException(
+                "Unable to get access token, It's likely that you provided an invalid credential "
+                "or your apiKey has expired. You can always generate a new apiKey "
+                "from your developer account"
+            )
+
+    @property
+    async def headers(self) -> dict:
+        return {**self.base_headers, "authorization": f"Bearer {await self.token}"}
+
+    async def api_call(
+        self, service_type: ServiceType, data: dict, method=HTTPMethod.POST
+    ):
+        http_method_call_kwargs = await self._parse_call_kwargs(
+            service_type=service_type, data=data
+        )
+        async with httpx.AsyncClient() as client:
+            http_method_callable = getattr(client, method.value.lower(), None)
+            if not http_method_callable:
+                raise UnsupportedHTTPMethodException(
+                    f"{method} is not a supported HTTP method"
+                )
+            try:
+                response = await http_method_callable(**http_method_call_kwargs)
+            except httpx.ConnectError:
+                raise ConnectionException(
+                    "Unable to connect to server. Please ensure you have an internet connection"
+                )
+            except httpx.ConnectTimeout:
+                raise ConnectionException("Server refused to respond")
+            return self._parse_response(response)
+
+    async def _parse_call_kwargs(self, service_type: ServiceType, data: dict) -> dict:
+        payload = {
+            "ServiceType": service_type,
+            "RequestRef": str(uuid4()),
+            "Data": data,
+        }
+        return {
+            "url": self.base_url,
+            "json": payload,
+            "headers": await self.headers,
+        }
---
+++ pykuda2/exceptions.py
@@ -0,0 +1,18 @@
+class UnsupportedHTTPMethodException(Exception):
+    ...
+
+
+class ConnectionException(Exception):
+    ...
+
+
+class ConnectionException(Exception):
+    ...
+
+
+class InvalidResponseException(Exception):
+    ...
+
+
+class TokenException(Exception):
+    ...
