1bc293234f77e9d2477264490c8ef66f3ec8eba7
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-04-10 16:03:50 +0100

    Add dev dependencies for documentation generation

--- pykuda2/__init__.py
+++ pykuda2/__init__.py
@@ -1,3 +1,9 @@
+"""
+A developer friendly wrapper for kuda API
+
+Modules exported by this package:
+- `utils`: Provides all the enums and data models used by PyKuda2
+"""
 from pykuda2.kuda import Kuda, AsyncKuda
 from pykuda2.utils import (
     Gender,
