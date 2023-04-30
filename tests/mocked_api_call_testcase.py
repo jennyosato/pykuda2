b550a6768e8d23528280b3ff5c139cc299411438
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-04-30 16:14:41 +0100

    Properly teardown patched methods in tests

--- tests/mocked_api_call_testcase.py
+++ tests/mocked_api_call_testcase.py
@@ -28,68 +28,88 @@ class MockedAPICallTestCase(TestDummyData, TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        get_patcher = patch("httpx.get")
-        post_patcher = patch("httpx.post")
-        put_patcher = patch("httpx.put")
-        patch_patcher = patch("httpx.patch")
-        delete_patcher = patch("httpx.delete")
-        options_patcher = patch("httpx.options")
-        head_patcher = patch("httpx.head")
+        cls.get_patcher = patch("httpx.get")
+        cls.post_patcher = patch("httpx.post")
+        cls.put_patcher = patch("httpx.put")
+        cls.patch_patcher = patch("httpx.patch")
+        cls.delete_patcher = patch("httpx.delete")
+        cls.options_patcher = patch("httpx.options")
+        cls.head_patcher = patch("httpx.head")
 
-        mock_get = get_patcher.start()
+        mock_get = cls.get_patcher.start()
         mock_get.return_value = cls.mocked_api_response
 
-        mock_post = post_patcher.start()
+        mock_post = cls.post_patcher.start()
         mock_post.return_value = cls.mocked_api_response
 
-        mock_put = put_patcher.start()
+        mock_put = cls.put_patcher.start()
         mock_put.return_value = cls.mocked_api_response
 
-        mock_patch = patch_patcher.start()
+        mock_patch = cls.patch_patcher.start()
         mock_patch.return_value = cls.mocked_api_response
 
-        mock_delete = delete_patcher.start()
+        mock_delete = cls.delete_patcher.start()
         mock_delete.return_value = cls.mocked_api_response
 
-        mock_options = options_patcher.start()
+        mock_options = cls.options_patcher.start()
         mock_options.return_value = cls.mocked_api_response
 
-        mock_head = head_patcher.start()
+        mock_head = cls.head_patcher.start()
         mock_head.return_value = cls.mocked_api_response
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.get_patcher.stop()
+        cls.post_patcher.stop()
+        cls.put_patcher.stop()
+        cls.patch_patcher.stop()
+        cls.delete_patcher.stop()
+        cls.options_patcher.stop()
+        cls.head_patcher.stop()
 
 
 class MockedAsyncAPICallTestCase(TestDummyData, IsolatedAsyncioTestCase):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        get_patcher = patch("httpx._client.AsyncClient.get")
-        post_patcher = patch("httpx._client.AsyncClient.post")
-        put_patcher = patch("httpx._client.AsyncClient.put")
-        patch_patcher = patch("httpx._client.AsyncClient.patch")
-        delete_patcher = patch("httpx._client.AsyncClient.delete")
-        options_patcher = patch("httpx._client.AsyncClient.options")
-        head_patcher = patch("httpx._client.AsyncClient.head")
+        cls.get_patcher = patch("httpx._client.AsyncClient.get")
+        cls.post_patcher = patch("httpx._client.AsyncClient.post")
+        cls.put_patcher = patch("httpx._client.AsyncClient.put")
+        cls.patch_patcher = patch("httpx._client.AsyncClient.patch")
+        cls.delete_patcher = patch("httpx._client.AsyncClient.delete")
+        cls.options_patcher = patch("httpx._client.AsyncClient.options")
+        cls.head_patcher = patch("httpx._client.AsyncClient.head")
 
-        mock_get = get_patcher.start()
+        mock_get = cls.get_patcher.start()
         mock_get.return_value = cls.mocked_api_response
 
-        mock_post = post_patcher.start()
+        mock_post = cls.post_patcher.start()
         mock_post.return_value = cls.mocked_api_response
 
-        mock_put = put_patcher.start()
+        mock_put = cls.put_patcher.start()
         mock_put.return_value = cls.mocked_api_response
 
-        mock_patch = patch_patcher.start()
+        mock_patch = cls.patch_patcher.start()
         mock_patch.return_value = cls.mocked_api_response
 
-        mock_delete = delete_patcher.start()
+        mock_delete = cls.delete_patcher.start()
         mock_delete.return_value = cls.mocked_api_response
 
-        mock_options = options_patcher.start()
+        mock_options = cls.options_patcher.start()
         mock_options.return_value = cls.mocked_api_response
 
-        mock_head = head_patcher.start()
+        mock_head = cls.head_patcher.start()
         mock_head.return_value = cls.mocked_api_response
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.get_patcher.stop()
+        cls.post_patcher.stop()
+        cls.put_patcher.stop()
+        cls.patch_patcher.stop()
+        cls.delete_patcher.stop()
+        cls.options_patcher.stop()
+        cls.head_patcher.stop()
 
 
 class CredentialMixin:
